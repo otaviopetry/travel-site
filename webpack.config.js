04be19a99b5cd4f0c75284d4c1bf73af737d325a
otaviopetry <otaviopetry@gmail.com>
2021-01-18 18:45:52 -0300

    feat: My first postcss @mixins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const postsCSSPlugins = [
     require('postcss-import'),
+    require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
