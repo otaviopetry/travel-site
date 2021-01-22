792da550e256d147b73b1568aed729562afdc957
otaviopetry <otaviopetry@gmail.com>
2021-01-22 16:46:54 -0300

    chore: RevealOnScroll class structure

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,9 +1,11 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 if (module.hot) {
     module.hot.accept();
 }
 
 let mobileMenu = new MobileMenu();
+let revealOnScroll = new RevealOnScroll();
 
