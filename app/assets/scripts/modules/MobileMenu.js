b0c2294fdd0a71a782683a37b36505e6ab17819a
otaviopetry <otaviopetry@gmail.com>
2021-01-22 15:46:35 -0300

    feat: Hamburguer icon animation

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -16,6 +16,7 @@ class MobileMenu {
     toggleMenu () {
         this.menuContent.classList.toggle('site-header__menu-content--is-visible');
         this.siteHeader.classList.toggle('site-header--is-expanded');
+        this.menuIcon.classList.toggle('site-header__menu-icon--close-x');
     }
 }
 
