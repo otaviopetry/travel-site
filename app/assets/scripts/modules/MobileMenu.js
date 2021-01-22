1aa58d9b7de90ef48de58ce630818667b8749f0a
otaviopetry <otaviopetry@gmail.com>
2021-01-22 15:11:07 -0300

    chore: Adjust MobileMenu class and styling

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ class MobileMenu {
     constructor () {
         this.menuIcon = document.querySelector('.site-header__menu-icon');
         this.menuContent = document.querySelector('.site-header__menu-content');
+        this.siteHeader = document.querySelector('.site-header');
 
         this.events();
     }
@@ -14,6 +15,7 @@ class MobileMenu {
     // Methods
     toggleMenu () {
         this.menuContent.classList.toggle('site-header__menu-content--is-visible');
+        this.siteHeader.classList.toggle('site-header--is-expanded');
     }
 }
 
