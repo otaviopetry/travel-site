e0b0249eafc2561899e61d2c1537d2b844c0a81d
otaviopetry <otaviopetry@gmail.com>
2021-01-22 23:16:13 -0300

    refactor: More performance improving
    Add lodash debounce to deal with browser resize listener
    
    Also add comments to the code

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,7 +1,9 @@
 import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 class RevealOnScroll {
     constructor () {
         this.itemsToReveal = document.querySelectorAll('.feature-item');
+        this.browserHeight = window.innerHeight;
         this.hideInitially();
         this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
@@ -10,18 +12,27 @@ class RevealOnScroll {
     // Events method
     events () {
         window.addEventListener('scroll', this.scrollThrottle);
+        
+        // Lodash debounce lets us call a function X miliseconds after the browser resize action ends
+        window.addEventListener('resize', debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     // Methods
     hideInitially () {
+        // Add the class to the items we want to reveal on scroll
+        // And make them initially invisible
         this.itemsToReveal.forEach(element => {
             element.classList.add('reveal-item');
             element.isRevealed = false;
         });
+        // Set a property to the last item, so we can check when the job is done
         this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
     }
 
     calcCaller () {
+        // If the element is still not revealed, calculate the remaining distance
         this.itemsToReveal.forEach(element => {
             if (!element.isRevealed) {
                 this.calculateIfScrolledTo(element);
@@ -30,14 +41,20 @@ class RevealOnScroll {
     }
 
     calculateIfScrolledTo (element) {
-        let scrollPercent = (element.getBoundingClientRect().y / window.innerHeight) * 100;
+        // To save processing, only executes if the element is about to appear on scroll
+        if (window.scrollY + this.browserHeight > element.offsetTop) {
+            // Get the percentage
+            let scrollPercent = (element.getBoundingClientRect().y / this.browserHeight) * 100;
 
-        if (scrollPercent < 100) {
-            element.classList.add('reveal-item--is-visible');
-            element.isRevealed = true;
-            
-            if (element.isLastItem) {
-                window.removeEventListener('scroll', this.scrollThrottle);
+            // If below desired, reveal it!
+            if (scrollPercent < 100) {
+                element.classList.add('reveal-item--is-visible');
+                element.isRevealed = true;
+                
+                // When the last item is revealed, remove the event listener to save processing
+                if (element.isLastItem) {
+                    window.removeEventListener('scroll', this.scrollThrottle);
+                }
             }
         }
     }
