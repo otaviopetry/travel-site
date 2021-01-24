d92cdff06535288ad0e49ec64a150e5358dd6833
otaviopetry <otaviopetry@gmail.com>
2021-01-24 12:01:28 -0300

    feat: Sticky header basic structure

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,12 +1,16 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 if (module.hot) {
     module.hot.accept();
 }
 
 let mobileMenu = new MobileMenu();
+
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 100);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 90);
 
+
+let stickyHeader = new StickyHeader();
