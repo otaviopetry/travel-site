21e12538cb6df9314d93127d9b060341fab150e1
otaviopetry <otaviopetry@gmail.com>
2021-02-06 18:18:56 -0300

    chore: Webpack basic build config

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 
 const postsCSSPlugins = [
@@ -9,35 +10,48 @@ const postsCSSPlugins = [
     require('autoprefixer')
 ];
 
-module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
+let config = {
+  entry: './app/assets/scripts/App.js',
+  module: {
+    rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader?url=false', {
+              loader: 'postcss-loader',
+              options: {
+                  postcssOptions: {
+                      plugins: postsCSSPlugins
+                  }
+              }
+          }]
+        }
+    ]
+  }
+};
+
+if (currentTask == 'dev') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  config.devServer = {
+    before: function (app, server) {
+        server._watch('./app/**/*.html');
     },
-    devServer: {
-        before: function (app, server) {
-            server._watch('./app/**/*.html');
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 3000,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader?url=false', {
-                    loader: 'postcss-loader',
-                    options: {
-                        postcssOptions: {
-                            plugins: postsCSSPlugins
-                        }
-                    }
-                }]
-            }
-        ]
-    }
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0'
+  },
+  config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  config.mode = 'production'
+}
+
+module.exports = config;
