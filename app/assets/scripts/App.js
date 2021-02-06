3b50ea69d705a91115c7d7ee720e97f8160c38ed
otaviopetry <otaviopetry@gmail.com>
2021-02-06 17:35:14 -0300

    chore: Split Modal.js to download only on demand
    Webpack automatically splits the conditional imports

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,17 +2,31 @@ import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
-import Modal from './modules/Modal';
+
+
+new MobileMenu();
+new RevealOnScroll(document.querySelectorAll('.feature-item'), 100);
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 90);
+new StickyHeader();
+let modal;
+
+document.querySelectorAll('.open-modal').forEach(element => {
+  element.addEventListener('click', event => {
+    event.preventDefault();
+    if (typeof modal == 'undefined') {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').
+        then( instance => {
+          modal = new instance.default();
+          setTimeout(() => modal.openTheModal(), 20);
+        }).
+        catch(() => console.log('There was a problem.'));
+    } else {
+      modal.openTheModal();
+    }
+  })
+})
+
 
 if (module.hot) {
     module.hot.accept();
 }
-
-let mobileMenu = new MobileMenu();
-
-new RevealOnScroll(document.querySelectorAll('.feature-item'), 100);
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 90);
-
-
-let stickyHeader = new StickyHeader();
-new Modal();
--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,17 +3,11 @@ class Modal {
     this.injectHTML();
     this.modal = document.querySelector('.modal');
     this.closeIcon = document.querySelector('.modal__close');
-    this.openModalButtons = document.querySelectorAll('.open-modal');
 
     this.events();
   }
 
   events() {
-    // Listen for open click
-    this.openModalButtons.forEach(button => {
-      button.addEventListener('click', event => this.openTheModal(event));
-    })
-
     // Listen for close click
     this.closeIcon.addEventListener('click', () => this.closeTheModal());
 
@@ -27,8 +21,7 @@ class Modal {
     }
   }
 
-  openTheModal(event) {
-    event.preventDefault();
+  openTheModal() {
     this.modal.classList.add('modal--is-visible');
   }
 
