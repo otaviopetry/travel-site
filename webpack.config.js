8b93f828754cc92c00c6edd9a22784b657751460
otaviopetry <otaviopetry@gmail.com>
2021-02-07 14:44:51 -0300

    chore: Implement Babel

--- webpack.config.js
+++ webpack.config.js
@@ -75,6 +75,16 @@ if (currentTask == 'dev') {
 }
 
 if (currentTask == 'build') {
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  })
   cssConfig.use.unshift(MiniCssExtractPlugin.loader);
   config.output = {
     filename: '[name].[chunkhash].js',
